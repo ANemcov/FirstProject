8dac70d8e4f040d71cd23dc08599992ce9584c1a
Alexandr Nemcov <alexnemcov@yandex.ru>
2014-09-14 21:04:36 +0400

    сщььше 2

--- FirstProject/FirstProjectTests/FirstProjectTests.h
+++ FirstProject/FirstProjectTests/FirstProjectTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Немцов Александр. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface FirstProjectTests : SenTestCase
+@interface FirstProjectTests : XCTestCase
 
 @end
--- FirstProject/FirstProjectTests/FirstProjectTests.m
+++ FirstProject/FirstProjectTests/FirstProjectTests.m
@@ -26,7 +26,7 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in FirstProjectTests");
+    XCTFail(@"Unit tests are not implemented yet in FirstProjectTests");
 }
 
 @end
